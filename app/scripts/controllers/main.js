ce857e6aa9c0fb9c60dd7a86f9f0492c54e3ce0d
Eric Miller <Eric@TheEricMiller.com>
2015-03-29 04:08:35 -0400

    Wired Beers to Firebase Controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,70 @@
  * Controller of the beerApp
  */
 angular.module('beerApp')
-  .controller('MainCtrl', function ($scope) {
+	.controller('MainCtrl', function($scope, fbutil, $timeout) {
 
-    $scope.beer = {	'name' : 'Piraat',
-    				'description' : 'Piraat is a Belgian pale ale, made by Van Steenberge brewery. It contains 10.5% alcohol, is hoppy, and is described as \"powerful\" with \"lots of subtle flavors.\"',
-    				'image_url' : 'https://coxrare.files.wordpress.com/2014/01/piraat.jpg'};
+		// synchronize a read-only, synchronized array of messages, limit to most recent 10
+		$scope.beers = fbutil.syncArray('beers', {
+			//limitToLast: 99
+		});
 
-	$scope.updateUserRating = function(beer, rating){
-		console.log('User rated ' + beer.name + ' ' + rating + ' stars.');
-	};
+		// display any errors
+		$scope.beers.$loaded().catch(alert);
 
-	$scope.skipBeer = function(beer){
-		console.log('User skipped ' + beer.name);
-	};
+		$scope.beers.$loaded().then(function() {
+			$scope.getBeer();
+		});
 
-	$scope.addBeerToDrinkList = function(beer){
-		console.log('User added ' + beer.name + ' to drink list.');
-	};
+		$scope.getBeer = function() {
+			$scope.beer = $scope.beers[Math.floor(Math.random() * $scope.beers.length)];
+			// check if user has already rated this beer?
+		};
 
-  });
+
+		// provide a method for adding a message
+		$scope.addBeer = function(newBeer) {
+			if (newBeer) {
+				// push a message to the end of the array
+				$scope.beers.$add({
+						name: newBeer
+					})
+					// display any errors
+					.catch(alert);
+			}
+		};
+
+
+		function alert(msg) {
+			$scope.err = msg;
+			$timeout(function() {
+				$scope.err = null;
+			}, 5000);
+		}
+
+		// $scope.beer = {	'name' : 'Piraat',
+		// 				'description' : 'Piraat is a Belgian pale ale, made by Van Steenberge brewery. It contains 10.5% alcohol, is hoppy, and is described as \"powerful\" with \"lots of subtle flavors.\"',
+		// 				'image_url' : 'https://coxrare.files.wordpress.com/2014/01/piraat.jpg'};
+
+		$scope.updateUserRating = function(beer, rating) {
+			console.log('User rated ' + beer.name + ' ' + rating + ' stars.');
+		};
+
+		$scope.skipBeer = function(beer) {
+			console.log('User skipped ' + beer.name);
+			// $scope.beer.labels.medium =
+			// 	'http://www.culinaryschools.org/clipart/beer/glass-of-beer.gif';
+			//TODO reset rating
+			//TODO reset image
+			$scope.getBeer();
+		};
+
+		$scope.addBeerToDrinkList = function(beer) {
+			console.log('User added ' + beer.name + ' to drink list.');
+		};
+
+	});
+
+
+// for (var i = 0; i < 3; i++) {
+//   console.log('TestLoop');
+// };
