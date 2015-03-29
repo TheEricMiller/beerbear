0a6991633c2f105e59336738cbba1092cda2389d
Eric Miller <Eric@TheEricMiller.com>
2015-03-29 05:13:05 -0400

    no message

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,70 +8,96 @@
  * Controller of the beerApp
  */
 angular.module('beerApp')
-	.controller('MainCtrl', function($scope, fbutil, $timeout) {
+  .controller('MainCtrl', function($scope, fbutil, $timeout) {
 
-		// synchronize a read-only, synchronized array of messages, limit to most recent 10
-		$scope.beers = fbutil.syncArray('beers', {
-			//limitToLast: 99
-		});
+    //$scope.user.uid = user.uid;
 
-		// display any errors
-		$scope.beers.$loaded().catch(alert);
+    // synchronize a read-only, synchronized array of messages, limit to most recent 10
+    $scope.beers = fbutil.syncArray('beers', {
+      //limitToLast: 99
+    });
 
-		$scope.beers.$loaded().then(function() {
-			$scope.getBeer();
-		});
+    // display any errors
+    $scope.beers.$loaded().catch(alert);
 
-		$scope.getBeer = function() {
-			$scope.beer = $scope.beers[Math.floor(Math.random() * $scope.beers.length)];
-			// check if user has already rated this beer?
-		};
+    $scope.beers.$loaded().then(function() {
+      $scope.getBeer();
+    });
 
-
-		// provide a method for adding a message
-		$scope.addBeer = function(newBeer) {
-			if (newBeer) {
-				// push a message to the end of the array
-				$scope.beers.$add({
-						name: newBeer
-					})
-					// display any errors
-					.catch(alert);
-			}
-		};
+    $scope.getBeer = function() {
+      $scope.beer = $scope.beers[Math.floor(Math.random() * $scope.beers.length)];
+      // check if user has already rated this beer?
+    };
 
 
-		function alert(msg) {
-			$scope.err = msg;
-			$timeout(function() {
-				$scope.err = null;
-			}, 5000);
-		}
+    // provide a method for adding a message
+    $scope.addBeer = function(newBeer) {
+      if (newBeer) {
+        // push a message to the end of the array
+        $scope.beers.$add({
+            name: newBeer
+          })
+          // display any errors
+          .catch(alert);
+      }
+    };
 
-		// $scope.beer = {	'name' : 'Piraat',
-		// 				'description' : 'Piraat is a Belgian pale ale, made by Van Steenberge brewery. It contains 10.5% alcohol, is hoppy, and is described as \"powerful\" with \"lots of subtle flavors.\"',
-		// 				'image_url' : 'https://coxrare.files.wordpress.com/2014/01/piraat.jpg'};
+    //------------------------------------------------------------
+    $scope.ratings = fbutil.syncArray('ratings');
+    $scope.ratings.$loaded().catch(alert);
+    $scope.rating = $scope.ratings[0];
 
-		$scope.updateUserRating = function(beer, rating) {
-			console.log('User rated ' + beer.name + ' ' + rating + ' stars.');
-		};
 
-		$scope.skipBeer = function(beer) {
-			console.log('User skipped ' + beer.name);
-			// $scope.beer.labels.medium =
-			// 	'http://www.culinaryschools.org/clipart/beer/glass-of-beer.gif';
-			//TODO reset rating
-			//TODO reset image
-			$scope.getBeer();
-		};
 
-		$scope.addBeerToDrinkList = function(beer) {
-			console.log('User added ' + beer.name + ' to drink list.');
-		};
+    //------------------------------------------------------------
 
-	});
+
+    $scope.updateUserRating = function(beer, rating) {
+      console.log('User rated ' + beer.name + ' ' + rating + ' stars.');
+
+      console.log($scope.user + ' ' + beer.id + ' ' + $scope.userBeerRating);
+
+      $scope.ratings.$add({
+          userID: 1,
+          beerID: 2,
+          userRating: rating,
+          created: Date.now()
+        })
+        // display any errors
+        .catch(alert);
+    };
+
+    $scope.skipBeer = function(beer) {
+      console.log('User skipped ' + beer.name);
+      // $scope.beer.labels.medium =
+      // 	'http://www.culinaryschools.org/clipart/beer/glass-of-beer.gif';
+      //TODO reset rating
+      //TODO reset image
+      //TODO if user rating exists pull it
+      $scope.getBeer();
+    };
+
+    $scope.addBeerToDrinkList = function(beer) {
+      console.log('User added ' + beer.name + ' to drink list.');
+    };
+
+
+    //------------------------------------------------------------
+
+    function alert(msg) {
+      $scope.err = msg;
+      $timeout(function() {
+        $scope.err = null;
+      }, 5000);
+    }
+
+  });
 
 
 // for (var i = 0; i < 3; i++) {
 //   console.log('TestLoop');
 // };
+
+// $scope.beer = {	'name' : 'Piraat',
+// 				'description' : 'Piraat is a Belgian pale ale, made by Van Steenberge brewery. It contains 10.5% alcohol, is hoppy, and is described as \"powerful\" with \"lots of subtle flavors.\"',
+// 				'image_url' : 'https://coxrare.files.wordpress.com/2014/01/piraat.jpg'};
